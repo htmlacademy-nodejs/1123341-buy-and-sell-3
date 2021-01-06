72fa97e1b41f27aa4186aa648c1fb75bbb5a1039
tyort <neotyo1986@gmail.com>
2021-01-06 16:19:45 +0300

    Учебный проект: покоритель файлов

--- src/service/console/generate.js
+++ src/service/console/generate.js
@@ -1,24 +1,42 @@
 'use strict';
 
-const {getRandomInt, shuffle} = require(`../../utils`);
-
 // ????????????????
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
 
-const {CATEGORIES, SENTENCES, TITLES, SumRestrict, OfferType, PictureRestrict} = require(`../../constants`);
+const {SumRestrict, OfferType, PictureRestrict} = require(`../../constants`);
+const {getRandomInt, shuffle} = require(`../../utils`);
+
+const FILE_SENTENCES_PATH = `../../data/sentences.txt`;
+const FILE_TITLES_PATH = `../../data/titles.txt`;
+const FILE_CATEGORIES_PATH = `../../data/categories.txt`;
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 
-// padstart: слово из 2-х букв, первый символ слева "number.toString()", оставшееся пространство заполняем нулями.
+// number.toString().padStart(2, 0) - слово состоит из 2-х символов
+// номер + оставшееся место заполняем нулями
 const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
 
-const generateOffers = (count) => (
+// возвращает либо массив элементов, либо пустой массив в случае ошибки
+const readContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+
+    // создаем массив из эелементов, разделенных переносом на новую строку (\n)
+    return content.split(`\n`);
+
+  } catch (err) {
+    console.error(chalk.red(err));
+    return [];
+  }
+};
+
+const generateOffers = (count, titles, categories, sentences) => (
   Array(count).fill({}).map(() => ({
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]],
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+    category: [categories[getRandomInt(0, categories.length - 1)]],
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
   }))
@@ -27,9 +45,14 @@ const generateOffers = (count) => (
 module.exports = {
   name: `--generate`,
   async run(args) {
-    const [count] = args; // получение первого элемента массива
+    const sentences = await readContent(FILE_SENTENCES_PATH);
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+
+
+    const [count] = args; // получение первого элемента массива с помощью деструктуризации
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer)); // преобразуем в строку JSON
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, sentences));
 
     try {
       // FILE_NAME - имя файла
