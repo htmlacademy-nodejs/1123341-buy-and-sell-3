1b4e60fa7cb3d6ba06ffcf5e8928b951c1a7d4b8
tyort <neotyo1986@gmail.com>
2021-01-13 01:21:59 +0300

    pug

--- src/express/index.js
+++ src/express/index.js
@@ -7,7 +7,7 @@ const mainRoutes = require(`./routes/main`);
 
 const DEFAULT_PORT = 8080;
 
-const app = express();
+const app = express(); // инициализируем новое веб-приложение (функция)
 
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
