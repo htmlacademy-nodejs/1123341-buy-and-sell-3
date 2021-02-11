708632b13af5dcdaa5956ad1127a7ae6caa89327
tyort_1986 <neotyo1986@gmail.com>
2021-02-11 14:28:10 +0300

    удаляем комментарии

--- src/express/routes/my.js
+++ src/express/routes/my.js
@@ -10,7 +10,6 @@ myRouter.get(`/`, async (req, res) => {
   res.render(`my-tickets`, {offers});
 });
 
-// комментарии к первым 3 публикациям с ресурса /api/offers/:offerId/comments;
 myRouter.get(`/comments`, async (req, res) => {
   const offers = await api.getOffers();
   res.render(`comments`, {offers: offers.slice(0, 3)});
