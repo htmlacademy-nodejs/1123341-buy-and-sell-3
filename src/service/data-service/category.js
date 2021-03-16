1954cb2f8e808a52ab45cf0ff0e73c875243a9db
tyort <neotyo1986@gmail.com>
2021-03-16 23:28:56 +0300

    Рефакторинг тестов категорий

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -14,12 +14,15 @@ class CategoryService {
   async findAll(needCount) {
     if (needCount) {
       const result = await this._Category.findAll({
+        // какие столбцы будут в результирующей таблице
         attributes: [
           `id`,
           `name`,
-          [Sequelize.fn(`COUNT`, `*`), `count`] // ??????????????
+          [Sequelize.fn(`COUNT`, `*`), `count`] // ????????создаем новый столбец по имени `count`. Дальше воспользуемся Sequelize.fn и применим агрегирующую функцию COUNT к сгруппированным строкам. В результате в столбце count окажется количество строк с данным id категории. А это то же самое, что количество объявлений в данной категории.
         ],
-        group: [Sequelize.col(`Category.id`)],
+        group: [Sequelize.col(`Category.id`)], // ??????????группируем строки сводной таблицы, с одинаковыми Category.id.
+
+        // подгружаем данные из таблицы связей
         include: [{
           model: this._OfferCategory,
           as: Aliase.OFFER_CATEGORIES,
@@ -27,6 +30,7 @@ class CategoryService {
         }]
       });
       return result.map((it) => it.get());
+
     } else {
       return this._Category.findAll({raw: true});
     }
